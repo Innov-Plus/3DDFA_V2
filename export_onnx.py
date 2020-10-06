c4071120bbf06e4c43c475657e5d520352a36021
ptphucbk <phan.tuanphuc@innov-plus.com>
2020-10-07 00:39:58 +0200

    Test pt model

--- export_onnx.py
+++ export_onnx.py
@@ -21,38 +21,50 @@ tddfa = TDDFA(gpu_mode=False, **cfg)
 x = torch.randn(batch_size, 3, 120, 120, requires_grad=True)
 torch_out = tddfa.model(x)
 
-traced_model = torch.jit.trace(tddfa.model, x)
-torch.jit.save(traced_model, "mb05_120x120.pt")
+# traced_model = torch.jit.trace(tddfa.model, x)
+# torch.jit.save(traced_model, "mb05_120x120.pt")
 
-model_dict = traced_model.state_dict()
+# model_dict = traced_model.state_dict()
 
-print(model_dict.keys())
+# print(model_dict.keys())
 
 # # Print model's state_dict
 # print("Model's state_dict:")
 # for param_tensor in model_dict:
 #     print(param_tensor, "\t", model_dict[param_tensor].size())
 
+pt_model = torch.jit.load("mb05_120x120.pt")
+pt_model.eval()
 
-# Export the model
-torch.onnx.export(tddfa.model,               # model being run
-                  # model input (or a tuple for multiple inputs)
-                  x,
-                  # where to save the model (can be a file or file-like object)
-                  "mb05_120x120.onnx",
-                  export_params=True,        # store the trained parameter weights inside the model file
-                  opset_version=10,          # the ONNX version to export the model to
-                  do_constant_folding=True,  # whether to execute constant folding for optimization
-                  input_names=['input'],   # the model's input names
-                  output_names=['output'])  # the model's output names
+pt_out = pt_model(x)
+
+def to_numpy(tensor):
+    return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+np.testing.assert_allclose(to_numpy(torch_out), to_numpy(pt_out), rtol=1e-03, atol=1e-05)
+
+print("Exported model has been tested with ONNXRuntime, and the result looks good!")
 
 
-onnx_model = onnx.load("mb05_120x120.onnx")
-passes = ['nop', 'eliminate_identity', 'eliminate_nop_transpose', 'eliminate_nop_pad', 'eliminate_unused_initializer',
-          'fuse_consecutive_squeezes', 'fuse_consecutive_transposes', 'fuse_add_bias_into_conv', 'fuse_transpose_into_gemm']
-optimized_model = optimizer.optimize(onnx_model, passes, fixed_point=True)
+# # Export the model
+# torch.onnx.export(tddfa.model,               # model being run
+#                   # model input (or a tuple for multiple inputs)
+#                   x,
+#                   # where to save the model (can be a file or file-like object)
+#                   "mb05_120x120.onnx",
+#                   export_params=True,        # store the trained parameter weights inside the model file
+#                   opset_version=10,          # the ONNX version to export the model to
+#                   do_constant_folding=True,  # whether to execute constant folding for optimization
+#                   input_names=['input'],   # the model's input names
+#                   output_names=['output'])  # the model's output names
 
-onnx.save(optimized_model, "mb05_120x120_opt.onnx")
+
+# onnx_model = onnx.load("mb05_120x120.onnx")
+# passes = ['nop', 'eliminate_identity', 'eliminate_nop_transpose', 'eliminate_nop_pad', 'eliminate_unused_initializer',
+#           'fuse_consecutive_squeezes', 'fuse_consecutive_transposes', 'fuse_add_bias_into_conv', 'fuse_transpose_into_gemm']
+# optimized_model = optimizer.optimize(onnx_model, passes, fixed_point=True)
+
+# onnx.save(optimized_model, "mb05_120x120_opt.onnx")
 
 
 # import onnx
